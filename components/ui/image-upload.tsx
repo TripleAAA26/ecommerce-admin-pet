2f3d762704eefc786a3d2790dd0c27914b612d59
TripleAAA26 <96.azim.96@gmail.com>
2024-05-31 15:27:49 +0500

    Product Entity(admin)

--- components/ui/image-upload.tsx
+++ components/ui/image-upload.tsx
@@ -48,7 +48,7 @@ export default function ImageUpload({ disabled, onChange, onRemove, value }: Ima
 
     return (
         <div>
-            <div className='mb-4 flex-col items-center gap-4'>
+            <div className='mb-4 flex items-center gap-4'>
                 {value.map(url =>
                     <div key={url} className='relative w-[200px] h-[200px] rounded-md overflow-hidden'>
                         <div className='z-10 absolute top-2 right-2'>
