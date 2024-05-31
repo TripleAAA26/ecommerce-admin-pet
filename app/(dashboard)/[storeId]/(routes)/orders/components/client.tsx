14fb01c5abf9b4e49fba9efc6580c336ad1699de
TripleAAA26 <96.azim.96@gmail.com>
2024-05-31 17:20:52 +0500

    Orders Entity(admin)

---
+++ app/(dashboard)/[storeId]/(routes)/orders/components/client.tsx
@@ -0,0 +1,26 @@
+'use client'
+
+import Heading from '@/components/ui/heading'
+import { Separator } from '@/components/ui/separator'
+import { DataTable } from '@/components/ui/data-table'
+
+import { OrderColumn, columns } from '@/app/(dashboard)/[storeId]/(routes)/orders/components/columns'
+
+interface OrderClientProps {
+    data: OrderColumn[]
+}
+
+export default function OrderClient({ data }: OrderClientProps) {
+
+    return (
+        <>
+            <Heading
+                title={`Orders (${data.length})`}
+                description="Manage orders for your store"
+            />
+            <Separator/>
+            <DataTable searchKey="products" columns={columns} data={data}/>
+        </>
+    )
+}
+
